a13d710926876a4a1eff6c51ac8ef56891afc3d6
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-05-16 18:10:41 +0200

    Add exception handling to duplicate restoration

--- theia_parse/util/duplicates.py
+++ theia_parse/util/duplicates.py
@@ -3,16 +3,32 @@ import shutil
 from pathlib import Path
 
 from theia_parse.const import DUPLICATE_SUFFIX, PARSED_JSON_SUFFIX
+from theia_parse.util.log import LogFactory
 
 
-def restore_duplicates(dir: Path) -> None:
+_log = LogFactory.get_logger()
+
+
+def restore_duplicates(dir: Path) -> int:
+    total = 0
     for root, _, file_names in os.walk(dir):
         for file_name in file_names:
             curr_path = Path(root) / file_name
             if "".join(curr_path.suffix) == DUPLICATE_SUFFIX:
                 source_path = Path(curr_path.read_text())
-                shutil.copy(
-                    source_path.with_suffix(PARSED_JSON_SUFFIX),
-                    curr_path.with_suffix(PARSED_JSON_SUFFIX),
-                )
+                try:
+                    shutil.copy(
+                        source_path.with_suffix(PARSED_JSON_SUFFIX),
+                        curr_path.with_suffix(PARSED_JSON_SUFFIX),
+                    )
+                except Exception:
+                    _log.warning(
+                        "Could not restore duplicates "
+                        "[source_path='{0}', current_path='{1}']",
+                        source_path,
+                        curr_path,
+                    )
                 os.remove(curr_path)
+                total += 1
+
+    return total
