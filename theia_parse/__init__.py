a0b55b26463d8d77d7a81d317ab127dc860ac163
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-05-16 14:27:14 +0200

    Add supported extensions

--- theia_parse/__init__.py
+++ theia_parse/__init__.py
@@ -1,6 +1,9 @@
 from theia_parse.model import ParserConfig
 from theia_parse.parser.directory_parser import DirectoryParser
 from theia_parse.parser.document_parser import DocumentParser
+from theia_parse.parser.file_parser import EXTENSION_TO_PARSER
 
 
-__all__ = ["DocumentParser", "ParserConfig", "DirectoryParser"]
+SUPPORTED_EXTENSIONS = list(EXTENSION_TO_PARSER.keys())
+
+__all__ = ["DocumentParser", "ParserConfig", "DirectoryParser", "SUPPORTED_EXTENSIONS"]
