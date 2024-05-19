67aaf22224be17f53e32edb723d95154760f8ebf
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-05-19 14:04:40 +0200

    Add prompt additions to init

--- theia_parse/__init__.py
+++ theia_parse/__init__.py
@@ -1,7 +1,13 @@
 from theia_parse.const import SUPPORTED_EXTENSIONS
-from theia_parse.model import ParserConfig
+from theia_parse.model import ParserConfig, PromptAdditions
 from theia_parse.parser.directory_parser import DirectoryParser
 from theia_parse.parser.document_parser import DocumentParser
 
 
-__all__ = ["DocumentParser", "ParserConfig", "DirectoryParser", "SUPPORTED_EXTENSIONS"]
+__all__ = [
+    "DocumentParser",
+    "ParserConfig",
+    "PromptAdditions",
+    "DirectoryParser",
+    "SUPPORTED_EXTENSIONS",
+]
