3a4b8ab421b2b79d2268dd580077b7cfd06cfba9
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-10-07 12:32:30 +0200

    Fix logger

--- theia_parse/util/log.py
+++ theia_parse/util/log.py
@@ -9,15 +9,18 @@ load_dotenv()
 
 log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
 is_debug = True if log_level in ["DEBUG", "TRACE"] else False
-logger.remove()
+logger.remove(0)
 logger.add(
     lambda msg: tqdm.write(msg, end=""),
     colorize=True,
-    format="{time} <light-blue>[{thread.name} | {module}]</light-blue> <level>{level}</level> {message}",
+    format=(
+        "{time} <light-blue>[{thread.name} | {module}]</light-blue> "
+        "<level>{level}</level> {message}"
+    ),
     level=log_level,
     backtrace=True,
     diagnose=is_debug,
-    filter=lambda record: record["extra"].get("name") == "default",
+    filter=lambda record: record["extra"].get("name") == "theia-parse",
 )
 
 
@@ -28,4 +31,4 @@ class LogFactory:
         Provides a logger with the given name and a preconfigured formatter.
         :return: logger instance
         """
-        return logger.bind(name="default")
+        return logger.bind(name="theia-parse")
